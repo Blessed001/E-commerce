f782475f765221f5ed09f9472e1c2648e2550292
diotec <claudiojoc46@mail.ru>
2018-12-21 01:29:06 +0300

    Class Departaments

---
+++ EcommerceApp/EcommerceApp/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace EcommerceApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EcommerceApp.Models.EcommerceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "EcommerceApp.Models.EcommerceContext";
+        }
+
+        protected override void Seed(EcommerceApp.Models.EcommerceContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ EcommerceApp/EcommerceApp/Models/EcommerceContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceApp.Models
+{
+    public class EcommerceContext:DbContext 
+    {
+        public EcommerceContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<EcommerceApp.Models.Departament> Departaments { get; set; }
+    }
+}
